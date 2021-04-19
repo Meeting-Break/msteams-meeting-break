8187b5544ae46012152862d4d72aa42649c28315
Nitish Sachar <nitish.sachar@protonmail.com>
2021-04-18 19:50:01 -0700

    add instructions

--- src/Components/SetBreakTime/SetBreakTime.tsx
+++ src/Components/SetBreakTime/SetBreakTime.tsx
@@ -1,4 +1,4 @@
-import { Form, FormButton, FormInput, Flex } from "@fluentui/react-northstar";
+import { Form, FormButton, FormInput, Flex, Text, FlexItem } from "@fluentui/react-northstar";
 import { Component, Fragment } from "react";
 import { ToSeconds } from "../../Utilities/BreakTimeConversionHelpers"
 
@@ -28,31 +28,34 @@ export class SetBreakTime extends Component<SetBreakTimeProps, SetBreakTimeState
     render() {
         return(
             <Fragment>
-                <Form onSubmit={this.onSubmit.bind(this)}>
-                    <Flex gap="gap.medium">
-                        <FormInput id="break-minutes"
-                                required
-                                label="Minutes"
-                                type="number"
-                                min={0}
-                                max={59}
-                                maxLength={2}
-                                onChange={(e) => {
-                                    this.setState({ minutes: Number((e.currentTarget as HTMLInputElement).value)});
-                                }}/>
-                        <FormInput id="break-seconds"
-                                required
-                                label="Seconds"
-                                type="number"
-                                min={0}
-                                max={59}
-                                maxLength={2}
-                                onChange={(e) => {
-                                    this.setState({ seconds: Number((e.currentTarget as HTMLInputElement).value)});
-                                }} />
-                    </Flex>
-                    <FormButton content="Start"/>
-                </Form>
+                <Flex gap="gap.medium" column>
+                    <Text content="How long is the break?"/>
+                    <Form onSubmit={this.onSubmit.bind(this)}>
+                        <Flex gap="gap.medium">
+                            <FormInput id="break-minutes"
+                                    required
+                                    label="Minutes"
+                                    type="number"
+                                    min={0}
+                                    max={59}
+                                    maxLength={2}
+                                    onChange={(e) => {
+                                        this.setState({ minutes: Number((e.currentTarget as HTMLInputElement).value)});
+                                    }}/>
+                            <FormInput id="break-seconds"
+                                    required
+                                    label="Seconds"
+                                    type="number"
+                                    min={0}
+                                    max={59}
+                                    maxLength={2}
+                                    onChange={(e) => {
+                                        this.setState({ seconds: Number((e.currentTarget as HTMLInputElement).value)});
+                                    }} />
+                        </Flex>
+                        <FormButton content="Start"/>
+                    </Form>
+                </Flex>
             </Fragment>
         )
     }
