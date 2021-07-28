6f35ad8fb3a342cd48a9410810c7f422f1918c14
uioporqwerty <nitish.sachar@protonmail.com>
2021-07-27 17:32:59 -0700

    log process

--- src/Utilities/Environment.ts
+++ src/Utilities/Environment.ts
@@ -3,5 +3,6 @@ import process from "process"
 const development = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 export function isDev() {
+    console.log(`process.env.NODE_ENV=${process.env.NODE_ENV}`)
     return development
 }
