3afe0bb0784b3e9f69273dbbeed75ee7eefee147
uioporqwerty <nitish.sachar@protonmail.com>
2021-07-28 23:29:58 -0700

    add graceful shutdown

--- public/index.js
+++ public/index.js
@@ -2,19 +2,26 @@ var express = require("express");
 var compression = require("compression");
 var helmet = require("helmet");
 
-var server = express();
+var app = express();
 
 var options = {
   index: "index.html",
 };
 
-server.use(compression());
-server.use(helmet());
-server.use("/", express.static("/home/site/wwwroot", options));
-server.use("/api/health", (req, res, next) => {
+app.use(compression());
+app.use(helmet());
+app.use("/", express.static("/home/site/wwwroot", options));
+app.use("/api/health", (req, res, next) => {
   res.sendStatus(200);
   return next();
 });
 console.log("NODE_ENV=" + process.env.NODE_ENV);
 console.log("Listening on port " + process.env.PORT + "...");
-server.listen(process.env.PORT);
+const server = app.listen(process.env.PORT);
+
+process.on("SIGTERM", () => {
+  console.debug("SIGTERM signal received: closing HTTP server");
+  server.close(() => {
+    console.log("HTTP server closed");
+  });
+});
